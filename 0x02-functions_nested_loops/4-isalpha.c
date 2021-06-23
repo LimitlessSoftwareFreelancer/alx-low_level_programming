f7dd884997a663f4f59439e561f777963614587d
T Hoosen <lsfreelancer8@gmail.com>
2021-06-23 20:02:42 +0000

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || ( c >= 99 && c <= 122))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}

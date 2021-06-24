0750de35bae05fd4c55541635a147434dae72775
T Hoosen <lsfreelancer8@gmail.com>
2021-06-24 17:33:36 +0000

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main (void)
+{
+	if (&positive_or_negative(i) == '0')
+	{
+		printf("%d is zero\n");
+	}
+
+	return (0);
+}
---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLEBRTON_H
+
+#include <stdio.h>
+
+void positive_or_negative(i);
+
+#endif /*HOLBERTON_H*/

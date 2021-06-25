a28cfa2fc576ae1dd987b7f81162cb17d26d64c7
T Hoosen <lsfreelancer8@gmail.com>
2021-06-25 16:46:44 +0000

    I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints numbers 0 to 9 followed by a newline
+ * 
+ */
+void print_numbers(void)
+{
+	char a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (a = 50 || a = 52)
+		{
+		continue;
+		}
+		else
+		{
+		_putchar(a);
+		}
+	}
+	_putchar('\n');
+	
+}

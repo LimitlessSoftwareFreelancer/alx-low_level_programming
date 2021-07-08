65d3b1f7f8aa55c248889c976206f39bc833db51
T Hoosen <lsfreelancer8@gmail.com>
2021-07-08 12:18:09 +0000

    You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 /**
- * factorial - returns the favtorial of a give number
- * Return Always 0.
- * function declared in header
+ * @factorial - returns the factorial of a give number
+ * @n - given number
+ * Return - factorial of n.
  */
 int factorial(int n)
 {
 
 	if (n == 0)
 	{
-		return(1);
+		return (1);
 	}
-	return (n * factorial(n - 1)); 
+	return (n * factorial(n - 1));
 }

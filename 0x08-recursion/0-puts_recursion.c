27736e4f012f935f75316e91dcfb18acf05c63d9
T Hoosen <lsfreelancer8@gmail.com>
2021-07-08 14:40:25 +0000

    She locked away a secret, deep inside herself, something she once knew to be true...but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 /**
- * @ - function _print_rev_recursion - prints a string in reverse
+ * @ - function _puts_recursion - prints a string in reverse
  * @ - *s - given string
- * Return - string in reverse
+ * Return - string
  *
  */
 void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_puts_recursion(s + 1);
+		_puts_recursion(s - 1);
 		_putchar(*s);
 	}
 }

a8aa0fdfc9f689b3b8921ae691403c51924c3d22
T Hoosen <lsfreelancer8@gmail.com>
2021-07-14 15:46:16 +0000

    Revert fail

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,17 +1,19 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * main -
- * @ :
- * @ :
+ * number_of_args - prints the number of arguments passed
+ * @argc : number of arg's
+ * @argv : index of arg's passed to program
  *
  * Return: Always 0.
  */
-int print_args(int argc, char* argv[])
+int number_of_args(int argc, char *argv[])
 {
 	int a;
 
 	for (a = 0; a < argc; a++)
+	{
 		printf("%s\n", argv[a]);
+	}
 	return 0;
 }

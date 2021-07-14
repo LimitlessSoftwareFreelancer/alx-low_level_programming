55fda33f2e233b739f6ce6a16f42d0b53a18a598
T Hoosen <lsfreelancer8@gmail.com>
2021-07-14 12:37:56 +0000

    It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - prints name of its executable file name
+ * @argc: amount of arguments //in this case we just need the first index
+ * @argv: the index of the parameter list //we can move through this, but again this is not the idea of the function
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+		{
+		printf("%s\n",argv[0]);
+		return 0;
+		}

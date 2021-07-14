9007830ddddec7ebd4937e342808348a35768387
T Hoosen <lsfreelancer8@gmail.com>
2021-07-14 19:12:46 +0000

    updates

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,27 +9,28 @@
  *
  *  Return: Always 0.
  */
-int mulligan(int argc, char *argv[])
-		{
-		int i,z,a,mul;
+int mulligan(int argc, char *argv[] __attribute__((unused)))
+{
+		int mul,a,z,i;
 
 		scanf("%d %d", &z, &a);
-		for (i = 1; i < argc; i++)
+
 		if (argc != 2)
 		{
 			printf("Error\n");
 		}
+		/*
 		else if (*argv[i] !> '0' && &argv[i] !< '9')
-		{
-			exit;
-		}
+		* {
+		*	exit;
+		* }
+		*/
 		else
-		{	
+		{
 			mul = z * a;
 
 			printf("%d", mul);
 		}
 		
 		return 0;
-		}
-		
+}

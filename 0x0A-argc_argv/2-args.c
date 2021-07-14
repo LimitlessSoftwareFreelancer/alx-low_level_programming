74f5bb4e98fc061a92a17379d6763428c9307e81
T Hoosen <lsfreelancer8@gmail.com>
2021-07-14 15:53:52 +0000

    reverted

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,13 +7,11 @@
  *
  * Return: Always 0.
  */
-int number_of_args(int argc, char *argv[])
+int print_args(int argc, char *argv[])
 {
 	int a;
-
+	
 	for (a = 0; a < argc; a++)
-	{
 		printf("%s\n", argv[a]);
-	}
 	return 0;
 }
